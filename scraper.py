64cb8c090bb1ebbc451429c39c10d8f2cca3d5cd
Daniel Nduati <dannduati2@gmail.com>
2021-10-19 02:24:21 +0300

    Update selenium scraper script
    I have no idea what has changed on this script i hould have checked
    before making the commit but i am too lazy and im revamping the project
    anyway with a new headless scraper so welp guess we dont need this
    anymore.

--- scraper.py
+++ scraper.py
@@ -1,8 +1,5 @@
-
 import time
-from twilio.rest import Client
 from selenium import webdriver
-import re
 
 class virus():
     def __init__(self):
@@ -12,17 +9,18 @@ class virus():
         try:
             self.driver.get('https://www.worldometers.info/coronavirus/')
             #//*[@id="main_table_countries_today"]/tbody[1]/tr[118]/td[1]
-            
             table = self.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]')
             country_element = table.find_element_by_xpath("//td[contains(., 'Kenya')]")
             row = country_element.find_element_by_xpath("./..")
             data = row.text.split(" ")
-            total_cases = data[1]
-            new_cases = data[2]
+            print(row.text)
+            print(data)
+            total_cases = data[2]
+            new_cases = data[3]
             total_deaths = data[3]
             new_deaths = data[4]
-            active_cases = data[5]
-            total_recovered = data[6]
+            active_cases = data[6]
+            total_recovered = data[5]
             serious_critical = data[7]
 
             print("Country: " + country_element.text)
