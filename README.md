9919c1e3a8b35adb7d2dd1b9568facd01ab18e28
Daniel Nduati <dannduati2@gmail.com>
2021-12-27 15:50:50 +0300

    Update : Directory restructure

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-# covid19-Kenya
-## Background
-### Selenium scraper
-### webscraper
-This is a basic webscrapper built on python3 and which collects real-time data from both official and unofficial sources so that the public can have a fair-minded understanding of this outbreak with transparent data sources.To pull data from these sources i use the selenium module,primarily it is for automating web applications for testing purposes but i use it to scrape data from: https://www.worldometers.info/coronavirus/
-
-### webscraper with message alert
-To add ontu the webscripting We will use the Twilio API to send the scraped data. Twilio is not a free service but they provide a trial account with some credit.In order to get started, we need to make an account in Twilio.
-https://www.twilio.com/try-twilio .Grab your Account SID and auth token from your Twilio account console.If you are on a Twilio Trial account, your outgoing SMS messages are limited to phone numbers that you have verified with Twilio. Phone numbers can be verified via your Twilio Console's Verified Caller IDs.
-
-## Headless scraper with bs4
-New and improved lol
+<h1 align="center">
+    <b>COVID19 KENYA</b>
+</h1>
--- scraper/new_scraper/scraper.py
+++ scraper/new_scraper/scraper.py
@@ -31,26 +31,8 @@ def store_db(k_data):
     engine = create_engine(database_uri)
     connection = engine.connect()
     Base.metadata.bind = engine
-    data = Data(
-		country_name = k_data[1],
-		total_cases = clean_column(k_data[2]),
-		new_cases = clean_column(k_data[3]),
-		total_deaths = clean_column(k_data[4]),
-		new_deaths = clean_column(k_data[5]),
-		total_recovered = clean_column(k_data[6]),
-		new_recovered = clean_column(k_data[7]),
-		active_cases = clean_column(k_data[8]),
-		serious_cases = clean_column(k_data[9]),
-		total_cases_per_m = clean_column(k_data[10]),
-		deaths_per_m = clean_column(k_data[11]),
-		total_tests = clean_column(k_data[12]),
-		tests_per_m = clean_column(k_data[13]),
-		population = clean_column(k_data[14]),
-		new_cases_per_m = clean_column(k_data[18])
-	)
+    data = Data(country_name = k_data[1],total_cases = clean_column(k_data[2]),new_cases = clean_column(k_data[3]),total_deaths = clean_column(k_data[4]),new_deaths = clean_column(k_data[5]),total_recovered = clean_column(k_data[6]),new_recovered = clean_column(k_data[7]),active_cases = clean_column(k_data[8]),serious_cases = clean_column(k_data[9]),total_cases_per_m = clean_column(k_data[10]),deaths_per_m = clean_column(k_data[11]),total_tests = clean_column(k_data[12]),tests_per_m = clean_column(k_data[13]),population = clean_column(k_data[14]),new_cases_per_m = clean_column(k_data[18]))
     Session = sessionmaker(bind=engine)
-    s = Session()
-
 
 def clean_column(no):
 	if no=='':
@@ -62,8 +44,8 @@ def clean_column(no):
 #store all countries' data to csv file and save the kenyan data to the database
 def store_data():
 	all_data,kenyan_data = get_data()
-	print(all_data[0])
-	print(kenyan_data)
+	#print(all_data[0])
+	#print(kenyan_data)
 	#frame = pd.DataFrame(all_data)
 	#date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
 	#frame.to_csv(f'data/data_{date}.csv',header=False, index=False,encoding='utf-8')
