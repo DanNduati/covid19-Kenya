4f33b739fc31bd841082928a00428b6f03b29526
Daniel Nduati <dannduati2@gmail.com>
2021-12-31 20:35:26 +0300

    Update config module

--- scraper/new_scraper/config.py
+++ scraper/new_scraper/config.py
@@ -1,5 +1,13 @@
 import os
+from os.path import join,dirname
 from dotenv import load_dotenv
 
-load_dotenv()
-database_uri = os.environ.get('DatabaseUri')
+dotenv_path = join(dirname(__file__),'.env')
+load_dotenv(dotenv_path,override=True)
+
+#database
+database_url = os.environ.get('DATABASE_URL')
+#twilio
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID',None)
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN',None)
+sender = os.environ.get('TWILIO_PHONE_NUMBER',None)
